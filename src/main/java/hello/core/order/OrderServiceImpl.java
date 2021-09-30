3d33c6d7595fe523e0a84b340bb56cc8f8f8f7e6
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-09-30 22:19:30 +0900

    의존관계 자동 주입 - 롬복 라이브러리 getter setter RequiredArgsConstructor 의 어노테이션을 추가하여 사용할 수 있다

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,20 +7,23 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component//("service")//충돌
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 /*
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 */
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
     //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
 
+    /* 해당내용은 final 필드값을 찾아서 autowired 된 생성자를 lombok의 @RequiredArgsConstructor가 만들어준다.
     @Autowired//생성자 주입은 단일 생성자일 경우 자동으로 autowired 가 된다
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         System.out.println("memberRepository = " + memberRepository);
@@ -28,6 +31,7 @@ public class OrderServiceImpl implements OrderService{
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+    */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
